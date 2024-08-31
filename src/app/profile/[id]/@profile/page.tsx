38d8a3ac22a256949c63e660ce23250a2c940152
Vlad Petryshyn <igor@bezruchko.com>
2024-08-31 18:31:42 +0200

    why it doesn't work on prod?

--- src/app/profile/[id]/@profile/page.tsx
+++ src/app/profile/[id]/@profile/page.tsx
@@ -13,40 +13,45 @@ interface Props {
 }
 
 export default async function Profile({ params, searchParams }: Props) {
-  const active = searchParams.content ?? "articles";
-  const headerList = headers();
-  const url = headerList.get("x-url");
+  try {
+    const active = searchParams.content ?? "articles";
+    const headerList = headers();
+    const url = headerList.get("x-url");
 
-  const resp = await fetch(`${url}/api/profile/${params.id}`, {
-    method: "GET",
-    cache: "no-store",
-    next: { tags: [createProfileTag(params.id)] },
-  });
+    const resp = await fetch(`${url}/api/profile/${params.id}`, {
+      method: "GET",
+      cache: "no-store",
+      next: { tags: [createProfileTag(params.id)] },
+    });
 
-  if (resp.status === 404 || resp.status === 500) return redirect(`${url}/404`);
-  const user = await resp.json();
+    if (resp.status === 404 || resp.status === 500)
+      return redirect(`${url}/404`);
+    const user = await resp.json();
 
-  return (
-    <section className="profile-container-profile-card">
-      <ProfileCard
-        username={user?.username}
-        id={String(user?._id)}
-        image={user?.image}
-      />
-      <div className="profile-container-profile-card-switches">
-        <Link
-          className={`profile-container-profile-card-switches-button ${active === "articles" && "active"}`}
-          href={`/profile/${user?._id}?content=articles`}
-        >
-          <h3>Articles</h3>
-        </Link>
-        <Link
-          className={`profile-container-profile-card-switches-button ${active === "workouts" && "active"}`}
-          href={`/profile/${user?._id}?content=workouts`}
-        >
-          <h3>Workouts</h3>
-        </Link>
-      </div>
-    </section>
-  );
+    return (
+      <section className="profile-container-profile-card">
+        <ProfileCard
+          username={user?.username}
+          id={String(user?._id)}
+          image={user?.image}
+        />
+        <div className="profile-container-profile-card-switches">
+          <Link
+            className={`profile-container-profile-card-switches-button ${active === "articles" && "active"}`}
+            href={`/profile/${user?._id}?content=articles`}
+          >
+            <h3>Articles</h3>
+          </Link>
+          <Link
+            className={`profile-container-profile-card-switches-button ${active === "workouts" && "active"}`}
+            href={`/profile/${user?._id}?content=workouts`}
+          >
+            <h3>Workouts</h3>
+          </Link>
+        </div>
+      </section>
+    );
+  } catch {
+    redirect("/404");
+  }
 }
