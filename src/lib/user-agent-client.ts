71575b85cb4da049035f65b370cf5a0451490bd9
Vlad Petryshyn <igor@bezruchko.com>
2024-09-02 15:00:24 +0200

    fixed build issue

--- src/lib/user-agent-client.ts
+++ src/lib/user-agent-client.ts
@@ -1,5 +1,5 @@
 "use client";
 export const isMobileDeviceClient = () => {
-  if (typeof window === "undefined") return window?.innerWidth < 760;
+  if (typeof window !== "undefined" && window) return window?.innerWidth < 760;
   return false;
 };
